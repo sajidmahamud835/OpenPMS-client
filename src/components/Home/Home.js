af836c427ebc6edfa064761e23571efdea20781c
coders-sohan <coders.sohan.0216@gmail.com>
2022-01-01 21:13:16 +0600

    branch test sohan

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,16 @@
 import React from "react";
 
 const Home = () => {
-    return (
-        <div>
-            <h1>Test</h1>
-            <p>This is anoher test.</p>
-
-        </div >
-    );
+	return (
+		<div>
+			<h1>Test</h1>
+			<p>
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda
+				explicabo possimus ratione reprehenderit aliquid amet sint consequuntur
+				atque, sunt deserunt?
+			</p>
+		</div>
+	);
 };
 
 export default Home;
