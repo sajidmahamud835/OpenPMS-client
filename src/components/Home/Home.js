0d5c97177c32f240f083fb8512d94b721af9c6c3
ShamsathMaleha <17203020@iubat.edu>
2022-01-01 21:56:22 +0600

    test2

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,6 +5,8 @@ const Home = () => {
 		<div>
 			<h1>Comming soon</h1>
 			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum dignissimos omnis sed, fugiat non perferendis? Placeat deserunt, quas officiis assumenda rerum atque doloremque officia laboriosam repudiandae! Suscipit incidunt laudantium est?</p>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci fugit, animi repellat sapiente ratione, provident id voluptatum tempore repellendus ipsum nulla. Autem, sed maxime. Omnis ut molestias ea quam veritatis.
+			</p>
 		</div>
 	);
 };
