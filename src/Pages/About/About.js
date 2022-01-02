31c670f6335bdff18e022748ed01f829b9a78c0b
Md-omar-faruke-programming-hero <farukeomar@gmail.com>
2022-01-02 18:53:33 +0600

    about us header added

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,13 +1,20 @@
 import React from "react";
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
+import "./About.css"
 
 const About = () => {
 	return (
 		<>
 			<Header />
-			<div className="container py-5 my-5">
-				<h1>This is about page</h1>
+			<div className="py-5 my-5">
+				<div>
+				    <h1 className="text-center fw-bolder about">About Us</h1>
+				</div>
+				<div>
+					<h2 className="my-3 text-center">Welcome to <span className="text-danger">OpenPMS</span></h2>
+				</div>
+				
 			</div>
 			<Footer />
 		</>
