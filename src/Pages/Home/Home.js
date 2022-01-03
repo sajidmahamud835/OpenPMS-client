62e235e2616c5c7b208adc64181553e1580deed2
sajidmahamud835 <sajidmahamud835@gmail.com>
2022-01-03 20:40:59 +0600

    removed unused imports, code format, code clean

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,7 +1,5 @@
 import React from "react";
 import FAQ from "../../components/FAQ/FAQ";
-import Footer from "../../components/Footer/Footer";
-import Header from "../../components/Header/Header";
 import Team from "../../components/Team/Team";
 import Banner from "./Banner/Banner";
 import HomeFeatures from "./HomeFeatures/HomeFeatures";
@@ -11,14 +9,14 @@ import HomeStatistic from "./HomeStatistic/HomeStatistic";
 const Home = () => {
 	return (
 		<>
-			
+
 			<Banner></Banner>
 			<HomeFeatures></HomeFeatures>
 			<HomeStatistic></HomeStatistic>
 			<HomePrice></HomePrice>
 			<FAQ></FAQ>
 			<Team></Team>
-			
+
 		</>
 	);
 };
