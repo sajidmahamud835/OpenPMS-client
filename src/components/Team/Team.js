88c8a922143a57694172ec995b5a94f59f4e10bd
Md-omar-faruke-programming-hero <farukeomar@gmail.com>
2022-01-03 19:22:54 +0600

    team member carousel added in home page

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -1,11 +1,33 @@
 import React from 'react';
 
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
 import member1 from "../../images/About _us/team-img1.jpg"
 import member2 from "../../images/About _us/team-img2.jpg"
 import member3 from "../../images/About _us/team-img3.jpg"
 import member4 from "../../images/About _us/team-img4.jpg"
 
 const Team = () => {
+    const responsive = {
+        superLargeDesktop: {
+          // the naming can be any, depends on you.
+          breakpoint: { max: 4000, min: 3000 },
+          items: 5
+        },
+        desktop: {
+          breakpoint: { max: 3000, min: 1024 },
+          items: 3
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 464 },
+          items: 2
+        },
+        mobile: {
+          breakpoint: { max: 464, min: 0 },
+          items: 1
+        }
+      };
     const members=[
 		{
 			"img":member1,
@@ -36,9 +58,17 @@ const Team = () => {
 					  <hr />
 
 					  <div className="row my-5 mx-0 px-0 container justify-content-center">
-						  {
+                      <Carousel 
+                 
+                            showDots={false}
+                            infinite={true}
+                            autoPlay={true}
+                            arrows={false}
+                            autoPlaySpeed={1000}
+                            responsive={responsive}>
+                           {
 							  members.map(member=> <div className="col-12 col-md-4 mt-3 ">
-							  <div className="shadow h-100 rounded teamcard   ">
+							  <div className="shadow h-100 rounded teamcard   " style={{width:"350px",height:"334px"}}>
 								<div className="memberspic">
 								  <img src={member.img} className="card-img-top memberPic  " alt="..."/>
 								</div>
@@ -54,6 +84,8 @@ const Team = () => {
 							  </div>
 							</div>)
 						  }
+                        </Carousel>   
+						 
 
 					  </div>
 
